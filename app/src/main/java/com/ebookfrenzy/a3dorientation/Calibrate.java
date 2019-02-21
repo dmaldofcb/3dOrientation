1a4cfbcfef8c95cd62ae970b1773c254d32bdd18
dmaldofcb <damald1@umbc.edu>
2019-02-21 18:02:03 -0500

    First commit

--- app/src/main/java/com/ebookfrenzy/a3dorientation/Calibrate.java
+++ app/src/main/java/com/ebookfrenzy/a3dorientation/Calibrate.java
@@ -9,5 +9,6 @@ public class Calibrate extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calibrate);
+        int test = 5;
     }
 }
